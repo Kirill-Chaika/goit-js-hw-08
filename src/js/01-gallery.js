b385f8116e02015a37353587751685125306b72f
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-06-03 23:15:27 +0300

    ssssa

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,6 @@ import '../css/01-gallery.css'
 import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items.js';
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Change code below this line
 
 const gallery = document.querySelector('.gallery');
 const markupGallery = createGallery(galleryItems);
